9a6b2503f837a19ce7a650b683b4efa5967a734f
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-09-23 18:28:31 +0200

    add Bracket and Kleisli instance, also to Dagger

--- modules/core/arrow-instances-data/src/main/kotlin/arrow/instances/kleisli.kt
+++ modules/core/arrow-instances-data/src/main/kotlin/arrow/instances/kleisli.kt
@@ -3,6 +3,8 @@ package arrow.instances
 import arrow.Kind
 import arrow.core.*
 import arrow.data.*
+import arrow.effects.typeclasses.Bracket
+import arrow.effects.typeclasses.ExitCase
 import arrow.instance
 import arrow.typeclasses.*
 
@@ -74,6 +76,47 @@ interface KleisliApplicativeErrorInstance<F, D, E> : ApplicativeError<KleisliPar
 
 @instance(Kleisli::class)
 interface KleisliMonadErrorInstance<F, D, E> : KleisliApplicativeErrorInstance<F, D, E>, MonadError<KleisliPartialOf<F, D>, E>, KleisliMonadInstance<F, D>
+
+@instance(Kleisli::class)
+interface KleisliBracketInstance<F, R, E> : Bracket<KleisliPartialOf<F, R>, E> {
+
+  fun BFE(): Bracket<F, E>
+
+  fun KME(): KleisliMonadErrorInstance<F, R, E>
+
+  override fun <A> just(a: A): Kleisli<F, R, A> =
+      KME().just(a)
+
+  override fun <A> Kind<KleisliPartialOf<F, R>, A>.handleErrorWith(f: (E) -> Kind<KleisliPartialOf<F, R>, A>): Kleisli<F, R, A> =
+      KME().run {
+        this@handleErrorWith.handleErrorWith(f)
+      }
+
+  override fun <A> raiseError(e: E): Kind<KleisliPartialOf<F, R>, A> = KME().raiseError(e)
+
+  override fun <A, B> Kind<KleisliPartialOf<F, R>, A>.flatMap(f: (A) -> Kind<KleisliPartialOf<F, R>, B>): Kleisli<F, R, B> =
+      KME().run {
+        this@flatMap.flatMap(f)
+      }
+
+  override fun <A, B> tailRecM(a: A, f: (A) -> Kind<KleisliPartialOf<F, R>, Either<A, B>>): Kleisli<F, R, B> =
+      KME().tailRecM(a, f)
+
+  override fun <A, B> Kind<KleisliPartialOf<F, R>, A>.bracketCase(
+      release: (A, ExitCase<E>) -> Kind<KleisliPartialOf<F, R>, Unit>,
+      use: (A) -> Kind<KleisliPartialOf<F, R>, B>): Kleisli<F, R, B> = BFE().run {
+    Kleisli { r ->
+      this@bracketCase.fix().run(r).bracketCase({ a, br ->
+        release(a, br).fix().run(r)
+      }, { a ->
+        use(a).fix().run(r)
+      })
+    }
+  }
+
+  override fun <A> Kind<KleisliPartialOf<F, R>, A>.uncancelable(): Kleisli<F, R, A> =
+      Kleisli { r -> BFE().run { this@uncancelable.fix().run(r).uncancelable() } }
+}
 
 /**
  * Alias for [Kleisli] for [Id]
